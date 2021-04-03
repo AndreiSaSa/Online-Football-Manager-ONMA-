5442aff5c8d818124fbd15905f0daecde7f17ca4
Andrei Sarmasan <andrei.sarmasan@nymphis.ro>
2021-04-03 14:00:54 +0300

    Added empty project, generated with SpringInitializr

---
+++ backend/com.onma/src/test/java/com/onma/OnlineManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
